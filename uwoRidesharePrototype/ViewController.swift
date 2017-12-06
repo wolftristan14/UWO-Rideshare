2449710eba12ea09395c087ddd70a32ec0dbc56c
Tristan Wolf <wolftristan@hotmail.com>
2017-12-06 13:38:14 -0500

    Initial Commit

---
+++ uwoRidesharePrototype/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  uwoRidesharePrototype
+//
+//  Created by Tristan Wolf on 2017-12-06.
+//  Copyright © 2017 Tristan Wolf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
