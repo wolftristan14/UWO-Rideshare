477a0d1d06e618aae79aa15c8f5a20cec79396a0
Tristan Wolf <wolftristan@hotmail.com>
2017-12-08 12:55:36 -0500

    first commit

---
+++ Pods/Target Support Files/Pods-uwoRidesharePrototype/Pods-uwoRidesharePrototype-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_uwoRidesharePrototype : NSObject
+@end
+@implementation PodsDummy_Pods_uwoRidesharePrototype
+@end
---
+++ uwoRidesharePrototype/ViewControllers/LoginViewController.swift
@@ -0,0 +1,9 @@
+//
+//  LoginViewController.swift
+//  uwoRidesharePrototype
+//
+//  Created by Tristan Wolf on 2017-12-06.
+//  Copyright © 2017 Tristan Wolf. All rights reserved.
+//
+
+import Foundation
