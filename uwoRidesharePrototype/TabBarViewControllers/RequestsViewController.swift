794a6542118769a10032d3c3a89375921678abd9
Tristan Wolf <wolftristan@hotmail.com>
2018-03-03 13:46:06 -0500

    added requests view controller (not reading from firebase yet)

---
+++ uwoRidesharePrototype/TabBarViewControllers/RequestsViewController.swift
@@ -0,0 +1,24 @@
+//
+//  RequestsViewController.swift
+//  uwoRidesharePrototype
+//
+//  Created by Tristan Wolf on 2018-03-03.
+//  Copyright © 2018 Tristan Wolf. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class RequestsViewController: UIViewController {
+    
+
+
+override func viewWillAppear(_ animated: Bool) {
+    
+    self.tabBarController?.navigationItem.title = "Requests"
+    
+    
+}
+
+}
